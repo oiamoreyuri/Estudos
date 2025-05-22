114d8bca7193839f3c1ef84b446a7b9c1a8c6d7b
Yuricos <yuricos3@gmail.com>
2025-05-22 12:30:14 -0300

    Inclusão de arquivos de estudos

---
+++ golang_do_zero/modulo4/tipos/tipo.go
@@ -0,0 +1,17 @@
+package main
+
+import "fmt"
+
+func main() {
+	//bool - true/false
+	fmt.Printf("Type: %T - Value: %v\n", true, true)
+
+	//string - sequênciq de bytes
+	fmt.Printf("Type: %T - Value: %v\n", "yuri", "yuri")
+
+	//int - números
+	fmt.Printf("Type: %T - Value: %v\n", 1, 1)
+
+	//float
+	fmt.Printf("Type: %T - Value: %v\n", 1.233, 1.233)
+}
---
+++ golang_do_zero/modulo4/varsconst/varconst.go
@@ -0,0 +1,39 @@
+package main
+
+func main() {
+	//Variáveis
+	// var nome string
+	// nome = "bento"
+	// fmt.Println(nome)
+
+	// var nome = "bento"
+	// fmt.Println(nome)
+
+	// f := "bento"
+	// fmt.Println(f)
+
+	// var idade int
+	// idade = 4
+	// fmt.Println(idade)
+
+	// var idade = 4
+	// fmt.Println(idade)
+
+	// f := 4
+	// fmt.Println(f)
+
+	// var tipo bool
+	// tipo = true
+	// fmt.Println(tipo)
+
+	// var tipo = true
+	// fmt.Println(tipo)
+
+	// f := true
+	// fmt.Println(f)
+
+	//Constantes
+	// const idadeBento = 4
+	// fmt.Println(idadeBento)
+
+}
